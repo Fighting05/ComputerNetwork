34d7f0a246734bea3c956bd76135ca4771ebca9e
Fighting05 <gjc20051205@163.com>
2025-10-23 11:42:37 +0800

    添加gui版本，位于gui-version/src/client.exe

---
+++ Lab1/gui-version/build/CMakeFiles/chat-gui.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/chat/gui-version/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/chat-gui.dir/imgui/backends/imgui_impl_glfw.cpp.obj" "gcc" "CMakeFiles/chat-gui.dir/imgui/backends/imgui_impl_glfw.cpp.obj.d"
+  "D:/chat/gui-version/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/chat-gui.dir/imgui/backends/imgui_impl_opengl3.cpp.obj" "gcc" "CMakeFiles/chat-gui.dir/imgui/backends/imgui_impl_opengl3.cpp.obj.d"
+  "D:/chat/gui-version/imgui/imgui.cpp" "CMakeFiles/chat-gui.dir/imgui/imgui.cpp.obj" "gcc" "CMakeFiles/chat-gui.dir/imgui/imgui.cpp.obj.d"
+  "D:/chat/gui-version/imgui/imgui_draw.cpp" "CMakeFiles/chat-gui.dir/imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/chat-gui.dir/imgui/imgui_draw.cpp.obj.d"
+  "D:/chat/gui-version/imgui/imgui_tables.cpp" "CMakeFiles/chat-gui.dir/imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/chat-gui.dir/imgui/imgui_tables.cpp.obj.d"
+  "D:/chat/gui-version/imgui/imgui_widgets.cpp" "CMakeFiles/chat-gui.dir/imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/chat-gui.dir/imgui/imgui_widgets.cpp.obj.d"
+  "D:/chat/gui-version/src/client_gui.cpp" "CMakeFiles/chat-gui.dir/src/client_gui.cpp.obj" "gcc" "CMakeFiles/chat-gui.dir/src/client_gui.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab1/gui-version/glfw/build/examples/CMakeFiles/boing.dir/boing.c.obj.d
@@ -0,0 +1,33 @@
+examples/CMakeFiles/boing.dir/boing.c.obj: \
+ D:\大三上\计算机网络\实验\Lab1\gui-version\glfw\examples\boing.c \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/stdio.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/corecrt.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/_mingw.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/vadefs.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/stdlib.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/corecrt_wstdlib.h \
+ D:/mingw64-posix/lib/gcc/x86_64-w64-mingw32/15.2.0/include/limits.h \
+ D:/mingw64-posix/lib/gcc/x86_64-w64-mingw32/15.2.0/include/syslimits.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/limits.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/malloc.h \
+ D:/mingw64-posix/lib/gcc/x86_64-w64-mingw32/15.2.0/include/mm_malloc.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/errno.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/math.h \
+ D:/大三上/计算机网络/实验/Lab1/gui-version/glfw/deps/glad/gl.h \
+ D:/mingw64-posix/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/stdint.h \
+ D:/mingw64-posix/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/stddef.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/string.h \
+ D:/mingw64-posix/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ D:/大三上/计算机网络/实验/Lab1/gui-version/glfw/include/GLFW/glfw3.h \
+ D:/大三上/计算机网络/实验/Lab1/gui-version/glfw/deps/linmath.h
